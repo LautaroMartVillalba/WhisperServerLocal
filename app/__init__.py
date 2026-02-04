7676c6bb1810a9ecc8f0ad1d9ddad3c9820a5aa4
Lautaro Villalba <lautaromartinvillalba@gmail.com>
2026-02-04 18:35:11 -0300

    delete: old python files

--- app/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-WhisperLocal API - Audio transcription service using Whisper model.
-"""
-
-__version__ = "0.1.0"
--- app/rabbitmq/__init__.py
+++
@@ -1,3 +0,0 @@
-"""
-RabbitMQ integration modules for message queue handling.
-"""
